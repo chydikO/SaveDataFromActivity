63ef05d51029b9b8733097c5eda6ae62f8da1808
MbPro13_chudikO <oleg.chudnovskiy.gmail.com>
2022-01-30 09:23:19 +0200

    add startIntent func

--- app/src/main/java/com/chudnovskiy/savedatafromactivity/MainActivity.kt
+++ app/src/main/java/com/chudnovskiy/savedatafromactivity/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.chudnovskiy.savedatafromactivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.util.Log
+import android.view.View
 import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private var mEditText: EditText? = null
@@ -33,5 +36,23 @@ class MainActivity : AppCompatActivity() {
         val savedMessage = mEditText?.text.toString()
         outState.putString(KEY, savedMessage)
         Log.d(TAG, "onSaveInstanceState: ".plus(savedMessage))
+    }
+
+    fun startIntent(view: View) {
+        if (!TextUtils.isEmpty(mEditText?.text.toString())) {
+            val textMessage = mEditText?.text.toString()
+            val sendIntent = Intent()
+            sendIntent.setAction(Intent.ACTION_SEND)
+            sendIntent.putExtra(Intent.EXTRA_TEXT, textMessage)
+            sendIntent.setType("text/plain")
+
+            val title = "Send"
+            val chooser: Intent = Intent.createChooser(sendIntent, title)
+            if (sendIntent.resolveActivity(packageManager) != null) {
+                startActivity(chooser)
+            }
+        } else {
+            Toast.makeText(this, "Empty text !!!", Toast.LENGTH_LONG).show()
+        }
     }
 }
